3989d18062f4388db0d7622157a88640535d0927
SrinivasV18 <srinivasv18@gmail.com>
2019-11-11 17:15:42 +0530

    changed to test cicd

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Srinivas Deployment in Azure';
+  title = 'Srinivas Nov Deployment in Azure';
 }
