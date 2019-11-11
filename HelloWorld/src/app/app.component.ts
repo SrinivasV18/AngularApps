5b72b10228e5ee8adedd5c7cceeab01ef3dbc6ed
SrinivasV18 <srinivasv18@gmail.com>
2019-11-11 14:22:05 +0530

    changed titlename

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Nivas Deployment in Azure';
+  title = 'Prapulla Deployment in Azure';
 }
