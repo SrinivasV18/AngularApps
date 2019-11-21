20bad78fdc88128f844557489336408fbec00bcc
SrinivasV18 <srinivasv18@gmail.com>
2019-11-21 19:41:50 +0530

    Update app.component.ts

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Srinivas Nov 20(5:55PM) Deployment in Azure';
+  title = 'Srinivas Nov 21(5:55PM) Deployment in Azure';
 }
